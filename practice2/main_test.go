13f50bf226afd94d93ce91573fdf3231b98685a9
xaeliudzyh <gtsagolov8@gmail.com>
2024-10-15 16:15:46 +0300

    done

--- practice2/main_test.go
+++ practice2/main_test.go
@@ -15,13 +15,9 @@ import (
 
 func TestInsertHandler(t *testing.T) {
 	mux := http.NewServeMux()
-	s := NewStorage(mux, "storage", []string{})
+	s := NewStorage(mux, "storage1", []string{"localhost:8082"}, true)
 	s.Run()
 	defer s.Stop()
-
-	r := NewRouter(mux, [][]string{{"storage"}})
-	r.Run()
-	defer r.Stop()
 
 	feature := geojson.NewFeature(orb.Point{rand.Float64(), rand.Float64()})
 	feature.ID = uuid.New().String()
@@ -29,37 +25,25 @@ func TestInsertHandler(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	req, err := http.NewRequest("POST", "/insert", bytes.NewReader(body))
+
+	req, err := http.NewRequest("POST", "/storage1/insert", bytes.NewReader(body))
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	rr := httptest.NewRecorder()
 	mux.ServeHTTP(rr, req)
-	if rr.Code == http.StatusTemporaryRedirect {
-		req, err := http.NewRequest("POST", rr.Header().Get("Location"), bytes.NewReader(body))
-		if err != nil {
-			t.Fatal(err)
-		}
-		rr := httptest.NewRecorder()
 
-		mux.ServeHTTP(rr, req)
-		if rr.Code != http.StatusOK {
-			t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
-		}
-	} else if rr.Code != http.StatusOK {
+	if rr.Code != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
 	}
 }
 
 func TestSelectHandler(t *testing.T) {
 	mux := http.NewServeMux()
-	s := NewStorage(mux, "storage", []string{})
+	s := NewStorage(mux, "storage1", []string{"localhost:8082"}, true)
 	s.Run()
 	defer s.Stop()
-
-	r := NewRouter(mux, [][]string{{"storage"}})
-	r.Run()
-	defer r.Stop()
 
 	// Вставляем объект для проверки
 	feature := geojson.NewFeature(orb.Point{1.0, 1.0})
@@ -68,62 +52,46 @@ func TestSelectHandler(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	reqInsert, err := http.NewRequest("POST", "/insert", bytes.NewReader(body))
+
+	reqInsert, err := http.NewRequest("POST", "/storage1/insert", bytes.NewReader(body))
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	rrInsert := httptest.NewRecorder()
 	mux.ServeHTTP(rrInsert, reqInsert)
-	if rrInsert.Code == http.StatusTemporaryRedirect {
-		reqInsert, err := http.NewRequest("POST", rrInsert.Header().Get("Location"), bytes.NewReader(body))
-		if err != nil {
-			t.Fatal(err)
-		}
-		rrInsert := httptest.NewRecorder()
-		mux.ServeHTTP(rrInsert, reqInsert)
-		if rrInsert.Code != http.StatusOK {
-			t.Errorf("Insert handler returned wrong status code: got %v want %v", rrInsert.Code, http.StatusOK)
-		}
+
+	if rrInsert.Code != http.StatusOK {
+		t.Errorf("Insert handler returned wrong status code: got %v want %v", rrInsert.Code, http.StatusOK)
 	}
 
-	req, err := http.NewRequest("GET", "/select?minX=0&minY=0&maxX=2&maxY=2", nil)
+	req, err := http.NewRequest("GET", "/storage1/select?minX=0&minY=0&maxX=2&maxY=2", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	rr := httptest.NewRecorder()
 	mux.ServeHTTP(rr, req)
-	if rr.Code == http.StatusTemporaryRedirect {
-		req, err := http.NewRequest("GET", rr.Header().Get("Location")+"?minX=0&minY=0&maxX=2&maxY=2", nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-		rr := httptest.NewRecorder()
 
-		mux.ServeHTTP(rr, req)
-		if rr.Code != http.StatusOK {
-			t.Errorf("Select handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
-		}
-		var fc geojson.FeatureCollection
-		if err := json.Unmarshal(rr.Body.Bytes(), &fc); err != nil {
-			t.Fatal(err)
-		}
-		if len(fc.Features) == 0 {
-			t.Errorf("Select handler returned no features, expected at least one")
-		}
-	} else if rr.Code != http.StatusOK {
+	if rr.Code != http.StatusOK {
 		t.Errorf("Select handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
+	}
+
+	var fc geojson.FeatureCollection
+	if err := json.Unmarshal(rr.Body.Bytes(), &fc); err != nil {
+		t.Fatal(err)
+	}
+
+	if len(fc.Features) == 0 {
+		t.Errorf("Select handler returned no features, expected at least one")
 	}
 }
 
 func TestReplaceHandler(t *testing.T) {
 	mux := http.NewServeMux()
-	s := NewStorage(mux, "storage", []string{})
+	s := NewStorage(mux, "storage1", []string{"localhost:8082"}, true)
 	s.Run()
 	defer s.Stop()
-
-	r := NewRouter(mux, [][]string{{"storage"}})
-	r.Run()
-	defer r.Stop()
 
 	feature := geojson.NewFeature(orb.Point{rand.Float64(), rand.Float64()})
 	feature.ID = uuid.New().String()
@@ -131,23 +99,17 @@ func TestReplaceHandler(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	// Вставляем объект
-	reqInsert, err := http.NewRequest("POST", "/insert", bytes.NewReader(body))
+
+	reqInsert, err := http.NewRequest("POST", "/storage1/insert", bytes.NewReader(body))
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	rrInsert := httptest.NewRecorder()
 	mux.ServeHTTP(rrInsert, reqInsert)
-	if rrInsert.Code == http.StatusTemporaryRedirect {
-		reqInsert, err := http.NewRequest("POST", rrInsert.Header().Get("Location"), bytes.NewReader(body))
-		if err != nil {
-			t.Fatal(err)
-		}
-		rrInsert := httptest.NewRecorder()
-		mux.ServeHTTP(rrInsert, reqInsert)
-		if rrInsert.Code != http.StatusOK {
-			t.Errorf("Insert handler returned wrong status code: got %v want %v", rrInsert.Code, http.StatusOK)
-		}
+
+	if rrInsert.Code != http.StatusOK {
+		t.Errorf("Insert handler returned wrong status code: got %v want %v", rrInsert.Code, http.StatusOK)
 	}
 
 	// Меняем координаты и делаем replace
@@ -156,37 +118,25 @@ func TestReplaceHandler(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	req, err := http.NewRequest("POST", "/replace", bytes.NewReader(body))
+
+	req, err := http.NewRequest("POST", "/storage1/replace", bytes.NewReader(body))
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	rr := httptest.NewRecorder()
 	mux.ServeHTTP(rr, req)
-	if rr.Code == http.StatusTemporaryRedirect {
-		req, err := http.NewRequest("POST", rr.Header().Get("Location"), bytes.NewReader(body))
-		if err != nil {
-			t.Fatal(err)
-		}
-		rr := httptest.NewRecorder()
-		mux.ServeHTTP(rr, req)
-		if rr.Code != http.StatusOK {
-			t.Errorf("Replace handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
-		}
-	} else if rr.Code != http.StatusOK {
+
+	if rr.Code != http.StatusOK {
 		t.Errorf("Replace handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
 	}
 }
 
 func TestDeleteHandler(t *testing.T) {
 	mux := http.NewServeMux()
-
-	s := NewStorage(mux, "storage", []string{})
+	s := NewStorage(mux, "storage1", []string{"localhost:8082"}, true)
 	s.Run()
 	defer s.Stop()
-
-	r := NewRouter(mux, [][]string{{"storage"}})
-	r.Run()
-	defer r.Stop()
 
 	feature := geojson.NewFeature(orb.Point{rand.Float64(), rand.Float64()})
 	feature.ID = uuid.New().String()
@@ -194,44 +144,57 @@ func TestDeleteHandler(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	// Вставляем объект
-	reqInsert, err := http.NewRequest("POST", "/insert", bytes.NewReader(body))
+
+	reqInsert, err := http.NewRequest("POST", "/storage1/insert", bytes.NewReader(body))
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	rrInsert := httptest.NewRecorder()
 	mux.ServeHTTP(rrInsert, reqInsert)
-	if rrInsert.Code == http.StatusTemporaryRedirect {
-		reqInsert, err := http.NewRequest("POST", rrInsert.Header().Get("Location"), bytes.NewReader(body))
-		if err != nil {
-			t.Fatal(err)
-		}
-		rrInsert := httptest.NewRecorder()
-		mux.ServeHTTP(rrInsert, reqInsert)
-		if rrInsert.Code != http.StatusOK {
-			t.Errorf("Insert handler returned wrong status code: got %v want %v", rrInsert.Code, http.StatusOK)
-		}
+
+	if rrInsert.Code != http.StatusOK {
+		t.Errorf("Insert handler returned wrong status code: got %v want %v", rrInsert.Code, http.StatusOK)
 	}
 
 	// Удаляем объект
-	req, err := http.NewRequest("POST", "/delete", bytes.NewReader(body))
+	req, err := http.NewRequest("POST", "/storage1/delete", bytes.NewReader(body))
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	rr := httptest.NewRecorder()
 	mux.ServeHTTP(rr, req)
 
-	if rr.Code == http.StatusTemporaryRedirect {
-		req, err := http.NewRequest("POST", rr.Header().Get("Location"), bytes.NewReader(body))
+	if rr.Code != http.StatusOK {
+		t.Errorf("Delete handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
+	}
+}
+
+func TestRedirectHandler(t *testing.T) {
+	mux := http.NewServeMux()
+	s := NewStorage(mux, "storage1", []string{"localhost:8082"}, true)
+	s.Run()
+	defer s.Stop()
+
+	// Имитация нескольких запросов для проверки редиректа
+	for i := 0; i < 4; i++ {
+		req, err := http.NewRequest("GET", "/storage1/select?minX=0&minY=0&maxX=2&maxY=2", nil)
 		if err != nil {
 			t.Fatal(err)
 		}
+
 		rr := httptest.NewRecorder()
 		mux.ServeHTTP(rr, req)
-		if rr.Code != http.StatusOK {
-			t.Errorf("Delete handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
+
+		if rr.Code == http.StatusTemporaryRedirect {
+			// Проверяем, что редирект происходит на правильный адрес
+			newLocation := rr.Header().Get("Location")
+			if newLocation == "" {
+				t.Error("Expected a redirection location, got none")
+			}
+		} else if rr.Code != http.StatusOK {
+			t.Errorf("Select handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
 		}
-	} else if rr.Code != http.StatusOK {
-		t.Errorf("Delete handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
 	}
 }
